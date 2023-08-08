a5b78ecd6b7bc39d980afd0e44b23be1fe37226e
Nicholas McEnroe <nsmcenro@gmail.com>
2023-08-08 09:08:55 -0700

    Feat: Add vendors that sell method

--- lib/market.rb
+++ lib/market.rb
@@ -8,4 +8,22 @@ class Market
   def add_vendor(vendor)
     @vendors << vendor
   end
+
+  def vendor_names
+    v_names = []
+    @vendors.each do |vendor|
+      v_names << vendor.name
+    end
+    v_names
+  end
+
+  def vendors_that_sell(item)
+    selling_vendors = []
+    @vendors.each do |vendor, inventory|
+      if vendor.inventory.include?(item)
+        selling_vendors << vendor
+      end
+    end
+    selling_vendors
+  end
 end
--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -17,6 +17,9 @@ RSpec.describe Market do
     @vendor2.stock(@item3, 25)
     @vendor3 = Vendor.new("Palisade Peach Shack")
     @vendor3.stock(@item1, 65)
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
   end
 
   describe "#initialize" do
@@ -30,10 +33,19 @@ RSpec.describe Market do
 
   describe '#add_vendors' do
     it "can add vendors to a market" do
-      @market.add_vendor(@vendor1)
-      @market.add_vendor(@vendor2)
-      @market.add_vendor(@vendor3)
       expect(@market.vendors).to eq([@vendor1, @vendor2, @vendor3])
+    end
+  end
+
+  describe '#vendor_names' do
+    it "can display the vendor names in a market" do
+      expect(@market.vendor_names).to eq(["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
+    end
+  end
+
+  describe '#vendors_that_sell' do
+    it "can return which vendors have a certain item" do
+      expect(@market.vendors_that_sell(@item1)).to eq([@vendor1, @vendor3])
     end
   end
 end
