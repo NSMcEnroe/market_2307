8e586767f368853f1e3d859109005d38a6bfbd6f
Nicholas McEnroe <nsmcenro@gmail.com>
2023-08-08 08:19:34 -0700

    Feat: Add vendor class

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -1,2 +1,13 @@
-# require './lib/item'
-# require './lib/vendor'
+require './lib/item'
+require './lib/vendor'
+
+RSpec.describe Vendor do
+  describe '#initialize' do
+    it "can initialize with all attributes" do
+      vendor = Vendor.new("Rocky Mountain Fresh")
+      expect(vendor).to be_a Vendor
+      expect(vendor.name).to eq("Rocky Mountain Fresh")
+    end
+  end
+end
+
