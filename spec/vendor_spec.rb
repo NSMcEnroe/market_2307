f8d4f5c5efa4947d43d3fa2c39f22ce5df7329c3
Nicholas McEnroe <nsmcenro@gmail.com>
2023-08-08 08:33:06 -0700

    Feat: Complete iteration 1

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -18,6 +18,34 @@ RSpec.describe Vendor do
       vendor = Vendor.new("Rocky Mountain Fresh")
       expect(vendor.check_stock(item1)).to eq(0)
     end
+
+    it "can interact with stock method" do
+      item1 = Item.new({name: 'Peach', price: "$0.75"})
+      item2 = Item.new({name: 'Tomato', price: '$0.50'})
+      vendor = Vendor.new("Rocky Mountain Fresh")
+      vendor.stock(item1, 30)
+      expect(vendor.check_stock(item1)).to eq(30)
+      vendor.stock(item1, 25)
+      expect(vendor.check_stock(item1)).to eq(55)
+      vendor.stock(item2, 12)
+      expect(vendor.check_stock(item2)).to eq(12)
+    end
   end
+
+  describe '#stock' do 
+    it "can add an item to the inventory hash" do
+      item1 = Item.new({name: 'Peach', price: "$0.75"})
+      item2 = Item.new({name: 'Tomato', price: '$0.50'})
+      vendor = Vendor.new("Rocky Mountain Fresh")
+      vendor.stock(item1, 30)
+      expect(vendor.inventory).not_to eq({})
+      vendor.stock(item1, 25)
+      vendor.stock(item2, 12)
+      expect(vendor.inventory).to include(item1, item2)
+    end
+  end
+
+
+
 end
 
