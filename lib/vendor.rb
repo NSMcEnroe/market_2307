dd7531fae8490d30c2188aee8c2c3f5740da8dff
Nicholas McEnroe <nsmcenro@gmail.com>
2023-08-08 09:20:28 -0700

    Add: Complete iteration 2

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -12,4 +12,19 @@ class Vendor
   def stock(item, amount)
     @inventory[item] += amount
   end
+
+  def potential_revenue
+    p_r = 0
+    @inventory.each do |item, amount|
+      p_r += item.price * amount
+    end
+    p_r
+  end
+
+
+
+  # def total_revenue
+  #   @rider_log.values.sum * @admission_fee
+  # end
+
 end
--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -46,6 +46,7 @@ RSpec.describe Market do
   describe '#vendors_that_sell' do
     it "can return which vendors have a certain item" do
       expect(@market.vendors_that_sell(@item1)).to eq([@vendor1, @vendor3])
+      expect(@market.vendors_that_sell(@item4)).to eq([@vendor2])
     end
   end
 end
