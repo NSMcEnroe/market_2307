22910c6d2ee1853bfe0856452aa2753f4cff705f
Nicholas McEnroe <nsmcenro@gmail.com>
2023-08-08 08:43:12 -0700

    Feat: Add market class

---
+++ spec/market_spec.rb
@@ -0,0 +1,14 @@
+require './lib/item'
+require './lib/vendor'
+require './lib/market'
+
+RSpec.describe Market do
+  describe "#initialize" do
+    it "can initialize and return attributes" do
+      market = Market.new("South Pearl Street Farmers Market")
+      expect(market).to be_a Market
+      expect(market.name).to eq("South Pearl Street Farmers Market")
+      expect(market.vendors).to eq([])
+    end
+  end
+end
